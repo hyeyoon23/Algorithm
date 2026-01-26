a7fb0308b201f7b56e13d3b712c3d8a6ec6281fe
hyeyoon23 <109073947+hyeyoon23@users.noreply.github.com>
2026-01-26 10:09:21 +0900

    [Silver V] Title: 폴리오미노, Time: 164 ms, Memory: 17588 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.java
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -4,21 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-        sc.close();
 
-        String response = "";
-
-        response = poliomino(s);
+        String response = poliomino(s);
         System.out.println(response);
     }
 
     private static String poliomino(String s) {
         String answer = "";
-        String A = "AAAA";
-        String B = "BB";
 
-        s = s.replaceAll("XXXX", A);
-        answer = s.replaceAll("XX", B);
+        s = s.replaceAll("XXXX", "AAAA");
+        answer = s.replaceAll("XX", "BB");
 
         if (answer.contains("X")) answer = "-1";
 
